e72ea9db2793009082b7ee697e56a4ed481d4433
Alok Sahay <alok.sahay87@gmail.com>
2024-07-14 07:35:15 +0200

    Added logging

--- WayBackMachine/WayBackMachine/ProductViewController.swift
+++ WayBackMachine/WayBackMachine/ProductViewController.swift
@@ -29,17 +29,19 @@ struct Timelog {
 class ProductViewController: UIViewController {
     
     
+    @IBOutlet weak var checkInButton: UIButton!
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var locationsLabel: UILabel!
     
     var timelogs = [Timelog]()
+    var logString = "Item #144" + "\n\n"
     
     let locations = ["fight.offer.airbag","pose.rewarded.glades", "usages.costs.shady", "owes.plans.brand", "tougher.useful.tickles"]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         itemImage.isHidden = true
+        checkInButton.isHidden = true
     }
     
     @IBAction func checkinComplete(_ sender: Any) {
@@ -48,7 +50,9 @@ class ProductViewController: UIViewController {
     
     func scanComplete() {
         itemImage.isHidden = false
+        checkInButton.isHidden = false        
         itemImage.image = .sneakers
+        locationsLabel.text = logString
     }
     
     func logTimeStamp() {
@@ -60,7 +64,8 @@ class ProductViewController: UIViewController {
     }
     
     func updateLocationList() {
-        var logString = ""
+        
+        logString = "Item #144" + "\n\n"
         
         for log in timelogs {
             logString.append("Timestamp: " + log.formattedTimestamp + ", Location: " + log.location + "\n\n")
